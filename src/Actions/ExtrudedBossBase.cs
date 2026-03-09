64f35d159c8501337d4dc54e613e99ada6aafb93
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2026-03-09 10:41:09 +0100

    added the basic actions to the parrent class

--- src/Actions/ExtrudedBossBase.cs
+++ src/Actions/ExtrudedBossBase.cs
@@ -3,44 +3,12 @@ namespace Loupedeck.SolidWorksPlugin
     using System;
 
     using Loupedeck.SolidWorksPlugin.Helpers;
-    public class ExtrudedBossBase : PluginDynamicCommand
+    public class ExtrudedBossBase : BasicSolidWorksAction
     {
         private readonly String _Icon;
         public ExtrudedBossBase()
-            : base(displayName: "Extruded Boss/Base", description: "Create an extruded boss/base", groupName: "Sketch Tools")
+            : base( "Extruded Boss/Base", "Create an extruded boss/base", "Sketch Tools", swCommands_e.swCommands_ExtrudedBossBase)
         {
         }
-
-        protected override void RunCommand(String actionParameter)
-        {
-            try
-            {
-                // get the SolidWorks application; exit if not available
-                if (!SolidWorksConnector.TryGetApplication(out var swApp))
-                {
-                    Console.WriteLine("SolidWorks is not running.");
-                    return;
-                }
-                Console.WriteLine("Connected to SolidWorks.");
-
-                // Attach to the active document
-                var model = (ModelDoc2)swApp.ActiveDoc;
-                if (model == null)
-                {
-                    Console.WriteLine("No active document in SolidWorks.");
-                    return;
-                }
-
-                swApp.RunCommand((Int32)swCommands_e.swCommands_ExtrudedBossBase, ""); // Run the Extruded Boss/Base command
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
-        }
-
-        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
-            => BitmapImage.FromResource(this.Plugin.Assembly, "Loupedeck.SolidWorksPlugin.ExtrudedBossBase.png");
     }
 }
--- src/Actions/SmartDimension.cs
+++ src/Actions/SmartDimension.cs
@@ -3,47 +3,12 @@ namespace Loupedeck.SolidWorksPlugin
     using System;
 
     using Loupedeck.SolidWorksPlugin.Helpers;
-    public class SmartDimension : PluginDynamicCommand
+    public class SmartDimension : BasicSolidWorksAction
     {
         private readonly String _Icon;
         public SmartDimension()
-            : base(displayName: "Smart Dimension", description: "Create a smart dimension", groupName: "Sketch Tools")
+            : base(displayName: "Smart Dimension", description: "Create a smart dimension", groupName: "Sketch Tools",swCommands_e.swCommands_SmartDimension)
         {
         }
-
-        protected override void RunCommand(String actionParameter)
-        {
-            try
-            {
-                // get the SolidWorks application; exit if not available
-                if (!SolidWorksConnector.TryGetApplication(out var swApp))
-                {
-                    Console.WriteLine("SolidWorks is not running.");
-                    return;
-                }
-                Console.WriteLine("Connected to SolidWorks.");
-
-                // Attach to the active document
-                var model = (ModelDoc2)swApp.ActiveDoc;
-                if (model == null)
-                {
-                    Console.WriteLine("No active document in SolidWorks.");
-                    return;
-                }
-                swApp.RunCommand((Int32)swCommands_e.swCommands_SmartDimension, ""); // Run the Smart Dimension command
-
-                // check if sketch is in sketch mode
-                Console.WriteLine("Entered sketch mode.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
-            this.ActionImageChanged();
-        }
-
-        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
-            => BitmapImage.FromResource(this.Plugin.Assembly, "Loupedeck.SolidWorksPlugin.SmartDimension.png");
-
     }
 }
